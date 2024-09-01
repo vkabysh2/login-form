964f33d8ee12fe5ceaf20c9da053ec10a290e3f4
Volodymyr Kabysh <v.kabishh@gmail.com>
2024-09-01 22:06:13 +0300

    types of react-hook-form is hell

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,13 +6,11 @@ import { Button } from "@/app/components";
 import {
     useForm,
     FormProvider,
-    SubmitHandler,
 } from "react-hook-form";
 import { loginFormSchema } from "@/app/loginFormSchema";
 import { PasswordInput } from "@/app/ui/password-input/password-input";
 import { EmailInput } from "@/app/ui/email-input/email-input";
 import { Rules } from "@/app/ui/rules/rules";
-import { Inputs } from "@/app/types";
 
 export default function LoginForm() {
     const { ...methods } = useForm({ resolver: yupResolver(loginFormSchema) });
@@ -24,7 +22,7 @@ export default function LoginForm() {
         console.log(event);
     }
 
-    const onSubmit: SubmitHandler<Inputs> = (data: any) => console.log(data);
+    const onSubmit = (data: any) => console.log(data);
 
     return (
         <main className="main">
