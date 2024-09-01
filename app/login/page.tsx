195d9a983d09d4212efd5b6de2436caa8e231f5b
Volodymyr Kabysh <v.kabishh@gmail.com>
2024-09-01 21:24:52 +0300

    remove Inputs type from login/page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,10 +10,9 @@ import { loginFormSchema } from "@/app/loginFormSchema";
 import { PasswordInput } from "@/app/ui/password-input/password-input";
 import { EmailInput } from "@/app/ui/email-input/email-input";
 import { Rules } from "@/app/ui/rules/rules";
-import { Inputs } from "@/app/types";
 
 export default function LoginForm() {
-    const { ...methods } = useForm<Inputs>({ resolver: yupResolver(loginFormSchema) });
+    const { ...methods } = useForm({ resolver: yupResolver(loginFormSchema) });
     const { touchedFields } = methods.formState;
     const keys = Object.keys(touchedFields);
     const disabled = keys.length === 0;
@@ -27,7 +26,7 @@ export default function LoginForm() {
 
     return (
         <main className="main">
-            <FormProvider <Inputs>{...methods}>
+            <FormProvider {...methods}>
                 <form className="main_form" onSubmit={methods.handleSubmit(onSubmit)}>
                     <h1 className="sing_up">
                         Sign up
